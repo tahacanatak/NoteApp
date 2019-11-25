79dd594b9ec3c72f93014cae76dcaf181205b9bd
tahacan.atak <tahacan.atak@gmail.com>
2019-11-25 16:52:06 +0300

    add note model

---
+++ NoteApp.API/Models/Note.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace NoteApp.API.Models
+{
+    public class Note
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [ForeignKey("Author")]
+        public string AuthorId { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime? CreatedTime { get; set; }
+
+        public DateTime? ModifiedTime { get; set; }
+
+
+
+
+        public ApplicationUser Author { get; set; }
+
+    }
+}
